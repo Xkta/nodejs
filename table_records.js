3f2dc3cbf3eaee4dea0b55b1ca8e9342db046409
zhangjiani <15646517+Xkta@users.noreply.github.com>
2025-11-03 19:24:05 +0800

    Update table_records.js

--- table_records.js
+++ table_records.js
@@ -1,84 +1,344 @@
+// const getTableRecords = () => {
+//   return {
+//     nextPageToken: "xxx",
+//     hasMore: false,
+//     records: [
+//       {
+//         primaryId: "record_1",
+//         data: {
+//           fid_1: "xxx",
+//           fid_2: 1343,
+//           fid_3: "a",
+//           fid_5: 1697994894000,
+//           fid_8: 1343.343443,
+//           fid_9: 40,
+//           fid_10: 3,
+//           fid_12: { name: "链接文字", url: "HTTPS://xxx" },
+//           fid_15: {
+//             ids: ["Wz0ob1A0jodZ1axAe1Obo7gvcRd"],
+//             idType: "DriveFile",
+//           },
+//           fid_16: {
+//             ids: ["7251866351663317012", "7271479319895998483"],
+//             idType: "ChatLarkID",
+//           },
+//           fid_17: {
+//             latitude: 39.903179,
+//             longitude: 116.397755,
+//           },
+//         },
+//       },
+//       {
+//         primaryId: "record_2",
+//         data: {
+//           fid_1: "yyy",
+//           fid_2: 222.33344,
+//           fid_3: "b",
+//           fid_4: ["b", "c"],
+//           fid_5: 1699999994000,
+//           fid_6: "xxx",
+//           fid_7: true,
+//           fid_8: 1343,
+//           fid_9: 80,
+//           fid_10: 8,
+//           fid_15: {
+//             ids: [
+//               "QPlEbvok0o8wTSxBAhGbkz2Ocgd",
+//               "NjvCbpB1OoHb6sxSJhKbJ4nHcGl",
+//               "SFAEbiE0Jo7wDRxres8bqlTwczh",
+//             ],
+//             idType: "DriveFile",
+//           },
+//           fid_16: {
+//             ids: ["7251866351663317012"],
+//             idType: "ChatLarkID",
+//           },
+//         },
+//       },
+//       {
+//         primaryId: "record_3",
+//         data: {
+//           fid_1: "yyy",
+//           fid_2: 222.33344,
+//           fid_3: "b",
+//           fid_5: 1699999994000,
+//           fid_8: 1343,
+//           fid_9: 80,
+//           fid_10: 8,
+//           fid_15: {
+//             ids: [
+//               "QPlEbvok0o8wTSxBAhGbkz2Ocgd",
+//               "NjvCbpB1OoHb6sxSJhKbJ4nHcGl",
+//               "SFAEbiE0Jo7wDRxres8bqlTwczh",
+//             ],
+//             idType: "DriveFile",
+//           },
+//           fid_16: {
+//             ids: ["7251866351663317012"],
+//             idType: "ChatLarkID",
+//           },
+//         },
+//       },
+//     ],
+//   };
+// };
 const getTableRecords = () => {
   return {
     nextPageToken: "xxx",
     hasMore: false,
     records: [
       {
-        primaryId: "record_1",
-        data: {
-          fid_1: "xxx",
-          fid_2: 1343,
-          fid_3: "a",
-          fid_5: 1697994894000,
-          fid_8: 1343.343443,
-          fid_9: 40,
-          fid_10: 3,
-          fid_12: { name: "链接文字", url: "HTTPS://xxx" },
-          fid_15: {
-            ids: ["Wz0ob1A0jodZ1axAe1Obo7gvcRd"],
-            idType: "DriveFile",
-          },
-          fid_16: {
-            ids: ["7251866351663317012", "7271479319895998483"],
-            idType: "ChatLarkID",
-          },
-          fid_17: {
-            latitude: 39.903179,
-            longitude: 116.397755,
-          },
-        },
-      },
-      {
-        primaryId: "record_2",
-        data: {
-          fid_1: "yyy",
-          fid_2: 222.33344,
-          fid_3: "b",
-          fid_4: ["b", "c"],
-          fid_5: 1699999994000,
-          fid_6: "xxx",
-          fid_7: true,
-          fid_8: 1343,
-          fid_9: 80,
-          fid_10: 8,
-          fid_15: {
-            ids: [
-              "QPlEbvok0o8wTSxBAhGbkz2Ocgd",
-              "NjvCbpB1OoHb6sxSJhKbJ4nHcGl",
-              "SFAEbiE0Jo7wDRxres8bqlTwczh",
-            ],
-            idType: "DriveFile",
-          },
-          fid_16: {
-            ids: ["7251866351663317012"],
-            idType: "ChatLarkID",
-          },
-        },
-      },
-      {
-        primaryId: "record_3",
-        data: {
-          fid_1: "yyy",
-          fid_2: 222.33344,
-          fid_3: "b",
-          fid_5: 1699999994000,
-          fid_8: 1343,
-          fid_9: 80,
-          fid_10: 8,
-          fid_15: {
-            ids: [
-              "QPlEbvok0o8wTSxBAhGbkz2Ocgd",
-              "NjvCbpB1OoHb6sxSJhKbJ4nHcGl",
-              "SFAEbiE0Jo7wDRxres8bqlTwczh",
-            ],
-            idType: "DriveFile",
-          },
-          fid_16: {
-            ids: ["7251866351663317012"],
-            idType: "ChatLarkID",
-          },
-        },
-      },
+                "primaryId": "dPRZPJWD7_zwA423Oh_ejIo_SdjFUX3glY2s0cC6y0c",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"上海市\",\"上海市\",\"黄浦区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "作业批改趣味贴纸",
+                    "fid_8": 1724136757000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "yOD5a00YB2B77hHRFnvvnck9cytd4CZ7WDVRElq6sXo",
+                "data": {
+                    "fid_1": "oryw542SrL1-MbQ2OZXj2sZgeUgk",
+                    "fid_2": "微信用户",
+                    "fid_3": "",
+                    "fid_4": "",
+                    "fid_5": "",
+                    "fid_6": "",
+                    "fid_7": "腾讯视频VIP会员月卡",
+                    "fid_8": 1758090422000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "TNFQZY4e0P5T2RHTSBL3bL63S9j__J5zURSj7T_iT1g",
+                "data": {
+                    "fid_1": "oryw542SrL1-MbQ2OZXj2sZgeUgk",
+                    "fid_2": "微信用户",
+                    "fid_3": "",
+                    "fid_4": "",
+                    "fid_5": "",
+                    "fid_6": "",
+                    "fid_7": "CS会员年卡",
+                    "fid_8": 1758080826000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "mOttLgT57IclUu9alavqjLciBIjCcZLNR6zpYmgc6Wk",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "蓝月亮洗衣液",
+                    "fid_8": 1719233616000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "sYADh2xlX_8mGEnfnr0Vu8y_SzeWAJuD6FpCQhruc7E",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"上海市\",\"上海市\",\"黄浦区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "环保手提袋",
+                    "fid_8": 1735023943000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "NTXlC_NdQ_egvd4jyQvJ8eVXECx8CO65kpGItY8YE54",
+                "data": {
+                    "fid_1": "oryw541lnyvCM8_klp84XuA4ZV_A",
+                    "fid_2": "",
+                    "fid_3": "琪钞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "天安门",
+                    "fid_6": "13013302263",
+                    "fid_7": "计时器",
+                    "fid_8": 1725621752000,
+                    "fid_9": 3
+                }
+            },
+            {
+                "primaryId": "AUcxoYokHQC_bbgIxaHRR9NfdeX1HM0WhNuxoIWMRvo",
+                "data": {
+                    "fid_1": "oryw541lnyvCM8_klp84XuA4ZV_A",
+                    "fid_2": "",
+                    "fid_3": "琪钞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "天安门",
+                    "fid_6": "13013302263",
+                    "fid_7": "环保手提袋",
+                    "fid_8": 1725621385000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "bmjE8K2qxxrRMENGPMYhnUfW9jC_7NfQZw4PRWyekyk",
+                "data": {
+                    "fid_1": "oryw541lnyvCM8_klp84XuA4ZV_A",
+                    "fid_2": "",
+                    "fid_3": "琪钞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "天安门",
+                    "fid_6": "13013302263",
+                    "fid_7": "计时器",
+                    "fid_8": 1725621751000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "3j2I83H08_y1_CO5uI_uUfqw9Rx9L4vW8R3ITz_mqWo",
+                "data": {
+                    "fid_1": "oryw541lnyvCM8_klp84XuA4ZV_A",
+                    "fid_2": "",
+                    "fid_3": "琪钞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "天安门",
+                    "fid_6": "13013302263",
+                    "fid_7": "作业批改趣味贴纸",
+                    "fid_8": 1725621375000,
+                    "fid_9": 2
+                }
+            },
+            {
+                "primaryId": "_oSpmq_YzeNB8cYk_qhJSI28ZjRNNvv_Lya5vESS8ZE",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "环保手提袋",
+                    "fid_8": 1719235426000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "OSDlMiR6Cl0ptrVBXoqe_hvhM9l0RxpyhpI_ilBDTtg",
+                "data": {
+                    "fid_1": "oryw542SrL1-MbQ2OZXj2sZgeUgk",
+                    "fid_2": "微信用户",
+                    "fid_3": "",
+                    "fid_4": "",
+                    "fid_5": "",
+                    "fid_6": "",
+                    "fid_7": "优酷VIP会员月卡",
+                    "fid_8": 1758090838000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "sjHXt_Y8Wak9zl6jPcGdkxzxiSjpdppWrVEPlp6QfD0",
+                "data": {
+                    "fid_1": "oryw542SrL1-MbQ2OZXj2sZgeUgk",
+                    "fid_2": "微信用户",
+                    "fid_3": "",
+                    "fid_4": "",
+                    "fid_5": "",
+                    "fid_6": "",
+                    "fid_7": "爱奇艺黄金会员月卡",
+                    "fid_8": 1758090806000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "4F8eTT_pw0n1x391wXGM2MdJ0Z9mxKju_VmQqvBocKE",
+                "data": {
+                    "fid_1": "oryw54wcdhiYrnb5F7iLO3VGCxsM",
+                    "fid_2": "",
+                    "fid_3": "王云飞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "你好",
+                    "fid_6": "18179650860",
+                    "fid_7": "CS会员年卡",
+                    "fid_8": 1736324620000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "slBDWK_eqt5ZJx5pHeUMzJIRcKu0xGJFejggHVmih98",
+                "data": {
+                    "fid_1": "oryw545VREL1iiD6qgJc2hSMyiXI",
+                    "fid_2": "微信用户",
+                    "fid_3": "咿呀呀",
+                    "fid_4": "[\"上海市\",\"上海市\",\"静安区\"]",
+                    "fid_5": "平顺路941弄",
+                    "fid_6": "15070582804",
+                    "fid_7": "cs会员年卡",
+                    "fid_8": 1725619368000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "t_Dd4jjJTgzBo3g_F6dHB7G9p_v6x_5yyhbOOguvjWo",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "cs会员年卡",
+                    "fid_8": 1719233562000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "qeTbDD7WNh2lNMUUjYWvimL3t_1K3ekZvv0te_vNXdk",
+                "data": {
+                    "fid_1": "oryw542SrL1-MbQ2OZXj2sZgeUgk",
+                    "fid_2": "微信用户",
+                    "fid_3": "",
+                    "fid_4": "",
+                    "fid_5": "",
+                    "fid_6": "",
+                    "fid_7": "瑞幸咖啡35元代金券",
+                    "fid_8": 1758090415000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "4gTyQy0LuoGLs_aO4Zl9qIKPd_fZr57peHGx7rGSSRY",
+                "data": {
+                    "fid_1": "oryw540LYFYf6liqCoaBpBoxNYbs",
+                    "fid_2": "",
+                    "fid_3": "测测",
+                    "fid_4": "[\"上海市\",\"上海市\",\"黄浦区\"]",
+                    "fid_5": "baba",
+                    "fid_6": "18179650860",
+                    "fid_7": "环保手提袋",
+                    "fid_8": 1735024336000,
+                    "fid_9": 1
+                }
+            },
+            {
+                "primaryId": "utLO4RmYYGZgF9Tj10P6t0PLmt4LqXbPTkxWE9pFV0g",
+                "data": {
+                    "fid_1": "oryw541lnyvCM8_klp84XuA4ZV_A",
+                    "fid_2": "",
+                    "fid_3": "琪钞",
+                    "fid_4": "[\"北京市\",\"北京市\",\"东城区\"]",
+                    "fid_5": "天安门",
+                    "fid_6": "13013302263",
+                    "fid_7": "环保手提袋",
+                    "fid_8": 1725621834000,
+                    "fid_9": 3
+                }
+            },
     ],
   };
 };
